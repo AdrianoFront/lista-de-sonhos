2f20de885345742a56e487a33faab1b164c18512
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-06-01 22:08:14 -0300

    function for selection active

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const Main = {
+
+  init: function () {
+    this.cacheSelectors()
+    this.bindEvents()
+  },
+  cacheSelectors: function () {
+this.checkButton = document.querySelectorAll('.check')
+  },
+
+  bindEvents: function () { 
+    const self = this
+    this.checkButtons.forEach(function (button) {
+      button.onclick = self.Events.checkButton_click 
+    })
+  }
+
+  Events: {
+    checkButton_click: function(){
+
+    }
+  }
+}
+
+Main.init{}
