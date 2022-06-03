742882fc9f3d498cc15d2a95d59004cdbd4420d1
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-06-02 21:35:01 -0300

    introduce function for assignment

--- js/main.js
+++ js/main.js
@@ -5,18 +5,26 @@ const Main = {
     this.bindEvents()
   },
   cacheSelectors: function () {
-this.checkButton = document.querySelectorAll('.check')
+this.$checkButton = document.querySelectorAll('.check')
   },
 
   bindEvents: function () { 
     const self = this
-    this.checkButtons.forEach(function (button) {
+    this.$checkButtons.forEach(function (button) {
       button.onclick = self.Events.checkButton_click 
     })
   }
 
   Events: {
-    checkButton_click: function(){
+    checkButton_click: function(e){
+      const li = e.target.parentElement
+      const isDone = li.classList.contains('done')
+      
+      if (!isDone) {
+        return li.classList.remove('done')
+      }
+      li.classList.add('done')
+      }
 
     }
   }
